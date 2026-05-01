47852ce6c99a62918b17bcf8165615d70dbdacba
Umadevi-2005 <ravisettyumadevi@gmail.com>
2026-05-01 10:46:04 +0530

    Add files via upload

---
+++ ems-backend2/src/main/java/net/javaguides/ems/dto/DepartmentDto.java
@@ -0,0 +1,41 @@
+package net.javaguides.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDto {
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    
+    public DepartmentDto(Long id2, String departmentName2, String departmentDescription2) {
+        this.id = id2;
+        this.departmentName = departmentName2;
+        this.departmentDescription = departmentDescription2;
+    }
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentDescription() {
+		return departmentDescription;
+	}
+	public void setDepartmentDescription(String departmentDescription) {
+		this.departmentDescription = departmentDescription;
+	}
+}
---
+++ ems-backend2/src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -0,0 +1,47 @@
+package net.javaguides.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+	public EmployeeDto(Long id2, String firstName2, String lastName2, String email2) {
+		this.id = id2;
+		this.firstName = firstName2;
+		this.lastName = lastName2;
+		this.email = email2;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
